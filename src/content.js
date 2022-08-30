f4a835595a8fafc70b8f109f9999793213e1f870
Hassam <111586718+OxygenOP@users.noreply.github.com>
2022-08-30 10:21:25 +0300

    chacked the event cycle

--- src/content.js
+++ src/content.js
@@ -1,32 +1,42 @@
 console.log("Hello world");
+let regex = /https:\/\/[a-z]*.aliexpress.com\/item\/[0-9]*.html/;
 
-let name = document.getElementsByClassName("product-title-text")[0].innerText;
-let image_uri = document.getElementById("poster");
-if (image_uri == null || image_uri == undefined) {
-  image_uri = document.getElementsByClassName("magnifier-image")[0];
+let currentUrl = window.location.href;
+let importantUrl = currentUrl.match(regex)[0];
+console.log("url", importantUrl);
+function onReq(request, sender, sendResponse) {
+  console.log(request);
+  if (importantUrl != null && importantUrl != undefined) {
+    let name =
+      document.getElementsByClassName("product-title-text")[0].innerText;
+    let image_uri = document.getElementById("poster");
+    if (image_uri == null || image_uri == undefined) {
+      image_uri = document.getElementsByClassName("magnifier-image")[0];
+    }
+
+    let price = document.getElementsByClassName("uniform-banner-box-price")[0]
+      .innerText;
+
+    let quantity = document
+      .getElementsByClassName("product-quantity-tip")[0]
+      .getElementsByTagName("span")[0].innerText;
+
+    console.log("image", image_uri.src);
+    console.log("name", name);
+
+    console.log("price", price);
+    console.log("quantity", quantity);
+
+    sendResponse({
+      uri: importantUrl,
+      name,
+      price,
+      stock: quantity,
+      image_uri: image_uri.src,
+    });
+  }
 }
 
-let price = document.getElementsByClassName("uniform-banner-box-price")[0]
-  .innerText;
-
-let quantity = document
-  .getElementsByClassName("product-quantity-tip")[0]
-  .getElementsByTagName("span")[0].innerText;
-
-console.log("image", image_uri.src);
-console.log("name", name);
-
-console.log("price", price);
-console.log("quantity", quantity);
-// let tab_uri;
-// chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-//     tab_uri = tabs[0].url;
-// });
-
-chrome.runtime.sendMessage(
-  { name, price, stock: quantity, image_uri: image_uri.src },
-
-  function (response) {
-    console.log("sending message");
-  }
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) =>
+  onReq(request, sender, sendResponse)
 );
--- src/popup.js
+++ src/popup.js
@@ -1,23 +1,34 @@
-function onReq(request, sender, sendResponse) {
-  ph = request;
-  console.log("msg: " + ph.name);
+document.getElementById("get-button").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { type: "getText" },
+      function (response) {
+        console.log(response);
+        ph = response;
 
-  document.getElementById("Container").style.display = "block";
-  document.getElementById("product-name").innerText = ph.name;
-  document.getElementById("product-price").innerText = ph.price;
-  document.getElementById("product-stock").innerText = ph.stock;
-  document.getElementById("product-img").src = ph.image_uri;
-  document.getElementById("product-image-uri").href = ph.image_uri;
-  document.getElementById(
-    "product-copy"
-  ).innerText = `${ph.name} ; ${ph.price} ; ${ph.stock} ; ${ph.image_uri}`;
-}
+        if (response != null && response != undefined) {
+          console.log("msg: " + ph.name);
+          document.getElementById("product-uri").href = ph.uri;
 
-chrome.runtime.onMessage.addListener(onReq);
+          document.getElementById("Container").style.display = "block";
+          document.getElementById("product-name").innerText = ph.name;
+          document.getElementById("product-price").innerText = ph.price;
+          document.getElementById("product-stock").innerText = ph.stock;
+          document.getElementById("product-img").src = ph.image_uri;
+          document.getElementById("product-image-uri").href = ph.image_uri;
+          document.getElementById(
+            "product-copy"
+          ).innerText = `${ph.name} ; ${ph.price} ; ${ph.stock} ; ${ph.uri} ; ${ph.image_uri}`;
+        }
+      }
+    );
+  });
+});
 
-function download() {
-  console.log("Gonna copy");
-  let text = document.getElementById("product-copy");
+function get() {
+  console.log("Gonna get");
+  // let text = document.getElementById("product-copy");
   // text.focus();
   // text.select();
   // try {
